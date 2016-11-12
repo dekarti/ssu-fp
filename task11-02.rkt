aad2645990553c2b97e4c3438e0cb5aa2b4b2b85
Arsen Arutyunyan <arutyunyanas@sgu.ru>
2016-11-12 18:16:42 +0300

    [TASK-11] Fix and complete task11

--- task11-02.rkt
+++ task11-02.rkt
@@ -1,46 +1,39 @@
 #lang racket
 
-;- inserts element 'e' into list 'L' at position 'i'
-(define (paste L e i) 
-  (define (paste-iter L cur N)
-    (if (null? L) N
-      (if (= cur i)
-        (paste-iter L (+ cur 1) (append N (cons e '())))
-        (paste-iter (cdr L) (+ cur 1) (append N (cons (car L) '()))))))
-  (paste-iter L 0 '()))
+;; Divides initial list onto two parts
+(define (cut L i k) 
+  (define (iter L1 cur L2 L3)
+    (cond 
+      ((< cur i) 
+       (iter (append L1 (list (car L3))) (+ cur 1) L2 (cdr L3)))
+      ((and (< cur (+ i k)) (>= cur i))
+       (iter L1 (+ cur 1) (append L2 (list (car L3))) (cdr L3)))
+      (else (list (append L1 L3) L2))))
+  (iter '() 0 '() L))
 
-;- moves i1 element into i2
-(define (move L i1 i2)
-  (define (get-iter L cur e N)
-    (if (= cur i1) 
-      (list (cdr L) (car L) N)
-      (get-iter (cdr L) (+ cur 1) e (append N (cons (car L) '())))))
-  
-  (define (paste-iter L2 e L1)
-    (let ((delta (- i2 i1)))
-      (if (> delta 0) (append L1 (paste L2 e delta))
-        (append (paste L1 e i2) L2)))) 
-
-  (apply paste-iter (get-iter L 0 1 '())))
-    
-
-(define (reorder1 L i1 k i2)
-  (if (= k 0) L
-    (reorder1 (move L (+ i1 (- k 1)) (+ i2 (- k 1)))
-             i1 (- k 1) i2)))
-
-(define (reorder2 L i1 cur k i2)
-  (if (= k cur) L
-    (reorder2 (move L (+ i1 cur) (+ i2 cur))
-             i1 (+ cur 1) k i2)))
+;; Inserts L2 into L1 on position i
+(define (insert L1 L2 i)
+  (define (divide cur L3 L4)
+    (if (= cur i) 
+        (list L3 L4)
+        (divide (+ cur 1)
+                (append L3 (list (car L4)))
+                (cdr L4))))
+  (let* ([parts (divide 0 '() L1)]
+         [first (car parts)]
+         [second (cadr parts)])
+    (append first L2 second)))
 
 (define (reorder L i1 k i2)
-  (let ((delta (- i2 i1)))
-    (if (> delta 0)
-      (reorder1 L i1 k i2)
-      (reorder2 L i1 0 k i2))))
+  (cond ((and (>= i2 i1) (< i2 (+ i1 k))) L)
+        ((> i1 i2) (apply
+          insert (append (cut L i1 k) (list i2))))
+        (else (apply
+          insert (append (cut L i1 k) (list (- i2 k)))))))
 
 
-(reorder '(0 1 2 3 4 5 6) 2 2 3) ;- (0 1 4 2 3 5 6)
-(reorder '(0 1 2 3 4 5 6) 0 3 3) ;- (3 4 5 0 1 2 6) 
-(reorder '(0 1 2 3 4 5 6) 3 2 0) ;- (0 1 4 2 3 5 6)
+(reorder '(0 1 2 3 4 5 6 7) 2 2 6)
+(reorder '(0 1 2 3 4 5 6 7) 2 1 3)
+(reorder '(0 1 2 3 4 5 6 7) 5 3 2)
+(reorder '(0 1 2 3 4 5 6 7) 2 2 8)
+(reorder '(a s d f g h j k) 3 3 2) ; (a s f g h d j k)
